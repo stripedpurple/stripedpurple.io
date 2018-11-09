a937e6f8abeafb02eee264354d75f1b191050cc1
Austin Barrett <abarrett@biohitech.com>
2018-11-08 19:24:06 -0500

    added tons of content

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -5,6 +5,29 @@
 
 $(document).ready(function () {
 
+    // Contact Us Submit
+    $('#contact-form').submit(function () {
+        return false;
+    });
+
+    $('#contact-submit').click(function () {
+
+        $.post('/contact', $('#contact-form').serialize(), function () {
+            console.log('success');
+            $('.footer .msg').html('<div class="notification is-success"><button class="delete"></button>Thank you for contacting Gold Medal Environmental. Someone will contact you shortly!</div>');
+        }).fail(function (err) {
+                console.log(err);
+                $('.footer .msg').html('<div class="notification is-danger"><button class="delete"></button>' + err.responseText + '</div>');
+            });
+
+        return false;
+    });
+
+    $(document).on('click', '.notification > button.delete', function () {
+        $(this).parent().addClass('is-hidden');
+        return false;
+    });
+
     // Check for click events on the navbar burger icon
     $(".navbar-burger").click(function () {
 
@@ -19,10 +42,10 @@ $(document).ready(function () {
         .not('[href="#"]')
         .not('[href="#0"]')
         .click(function (event) {
-                var $position = $($(this).attr('href')).offset().top;
-                $('html, body').stop().animate({
-                    scrollTop: $position
-                }, 600);
-                event.preventDefault();
-            });
+            var $position = $($(this).attr('href')).offset().top;
+            $('html, body').stop().animate({
+                scrollTop: $position
+            }, 600);
+            event.preventDefault();
+        });
 });
