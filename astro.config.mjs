626ed02cceacf3faaed3cc9e31fe5fbfdc08008f
Austin Barrett <virus.cmd@gmail.com>
2022-09-19 00:56:09 -0400

    base layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+
+import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), sitemap(), prefetch()]
+});
