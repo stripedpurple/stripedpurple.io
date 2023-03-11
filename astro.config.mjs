41eced90df1f61d166397c8da2673c7653930027
Austin Barrett <virus.cmd@gmail.com>
2023-03-11 13:11:31 -0500

    mobile tweaks and review copy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import prefetch from "@astrojs/prefetch";
-
 import netlify from "@astrojs/netlify/functions";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), prefetch()],
+  integrations: [tailwind(), sitemap(), prefetch(), mdx()],
   output: "server",
-  adapter: netlify(),
+  adapter: netlify()
 });
